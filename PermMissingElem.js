76dd3789301b8b03e785117cc8200f4a6a6ec862
Wangjun Yang <wangjun1yang@gmail.com>
2018-06-07 09:09:22 +0300

    Update PermMissingElem.js

--- PermMissingElem.js
+++ PermMissingElem.js
@@ -10,3 +10,13 @@ for (let i = 0; i < B.length; i++) {
  } 
 return x+1;
 }
+
+function solution(A) {
+    let B = A.length +1;
+    let sum = B + B*(B-1)*0.5 ;
+    let sumMinusMissing = 0;
+    for (i = 0; i < A.length; i++) { 
+        sumMinusMissing += A[i];
+    }
+    return sum-sumMinusMissing;
+}
