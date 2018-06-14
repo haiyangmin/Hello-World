2e2b30221f6e01a92e73b88aa9c9896b2e492a3a
Wangjun Yang <wangjun1yang@gmail.com>
2018-06-14 13:30:55 +0300

    Update MaxSliceSum.js

--- MaxSliceSum.js
+++ MaxSliceSum.js
@@ -1,5 +1,6 @@
 
-A non-empty array A consisting of N integers is given. A pair of integers (P, Q), such that 0 ≤ P ≤ Q < N, is called a slice of array A. The sum of a slice (P, Q) is the total of A[P] + A[P+1] + ... + A[Q].
+A non-empty array A consisting of N integers is given. A pair of integers (P, Q), such that 0 ≤ P ≤ Q < N, is called a slice of array A.
+The sum of a slice (P, Q) is the total of A[P] + A[P+1] + ... + A[Q].
 
 Write a function:
 
