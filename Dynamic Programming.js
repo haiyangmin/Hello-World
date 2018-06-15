194a899010057cd888204b094c6f9caad7aed868
Wangjun Yang <wangjun1yang@gmail.com>
2018-06-15 13:36:05 +0300

    Update Dynamic Programming.js

--- Dynamic Programming.js
+++ Dynamic Programming.js
@@ -53,6 +53,51 @@ var amount = 19;
 
 alert(waysToReturnMemoize(amount,denominations));
 
+
+Let's look at a sample problem:
+
+Let us say that you are given a number N, you've to find the number of different ways to write it as the sum of 1, 3 and 4.
+
+For example, if N = 5, the answer would be 6.
+
+1 + 1 + 1 + 1 + 1
+1 + 4
+4 + 1
+1 + 1 + 3
+1 + 3 + 1
+3 + 1 + 1
+Sub-problem: DPn be the number of ways to write N as the sum of 1, 3, and 4.
+Finding recurrence: Consider one possible solution, n = x1 + x2 + ... xn. If the last number is 1, the sum of the remaining numbers should be n - 1. So, number of sums that end with 1 is equal to DPn-1.. Take other cases into account where the last number is 3 and 4. The final recurrence would be:
+
+DPn = DPn-1 + DPn-3 + DPn-4.
+
+Take care of the base cases. DP0 = DP1 = DP2 = 1, and DP3 = 2.
+
+Implementation:
+
+ DP[0] = DP[1] = DP[2] = 1; DP[3] = 2;
+    for (i = 4; i <= n; i++) {
+      DP[i] = DP[i-1] + DP[i-3] + DP[i-4];
+    }
+
+function solution(B){
+  let n = B;
+  let result=[];
+    result[0] = 0;
+    result[1]= 1;
+    result[2]= 2;
+    result[3]= 3 ;
+     for (let i = 4; i <= n; i++) {
+      result[i] = result[i-1] + result[i-2] + result[i-3];
+    }
+    return result[n];
+    }
+    
+  var A = [1, 2, 3];
+var B = 5;
+alert(solution(B));
+
+
 Example 2
 A game for one player is played on a board consisting of N consecutive squares, numbered from 0 to N − 1.
 There is a number written on each square. A non-empty array A of N integers contains the numbers written on the squares. Moreover, 
